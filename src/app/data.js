aacc1f560082a38d3f642280622046fd0c9f9984
Junha park <grd0716@gmail.com>
2023-11-01 14:20:16 +0900

    feat: image viewer

---
+++ src/app/data.js
@@ -0,0 +1,33 @@
+export const illustrationsList = [{
+    name: '趣味の違い',
+    artist: 'Anmi',
+    description: 'エレガントガール',
+    url: 'https://pbs.twimg.com/media/F7CV_wjWoAAqf7a?format=jpg&name=large',
+    alt: 'Anmi 趣味の違い'
+  }, {
+    name: '電車でこんなカップルを見たのさ',
+    artist: 'Anmi',
+    description: '疲れた。。。',
+    url: 'https://pbs.twimg.com/media/F3KKPoWaIAAerdA?format=jpg&name=large',
+    alt: 'Anmi 電車でこんなカップルを見たのさ'
+  }, {
+    name: '夏も終わり',
+    artist: 'Anmi',
+    description: '夏の終わりは君が恋しい',
+    url: 'https://pbs.twimg.com/media/Fb1ZQZBUsAA4IgZ?format=jpg&name=large',
+    alt: 'Anmi 夏も終わり'
+  }, {
+    name: '月と睫毛',
+    artist: '凪',
+    description: '少しの光',
+    url: 'https://pbs.twimg.com/media/Fy0kBVOagAAQEv2?format=jpg&name=large',
+    alt: '凪 月と睫毛'
+  },{
+    name: '夕立',
+    artist: '凪',
+    description: '夕立、君と一緒に',
+    url: 'https://pbs.twimg.com/media/F6oKICobgAAXEgq?format=jpg&name=large',
+    alt: '凪 夕立'
+  },
+
+];
